385a146ab499984bc2fc551c91e0dd01d90fa981
RaphaelReiter <rdso.reiter@gmail.com>
2017-06-15 12:09:33 +0200

    sign in ui custom classes complete

---
+++ firstSocialMediaApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  firstSocialMediaApp
+//
+//  Created by Raphael Reiter on 14/06/2017.
+//  Copyright © 2017 Raphael Reiter. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0

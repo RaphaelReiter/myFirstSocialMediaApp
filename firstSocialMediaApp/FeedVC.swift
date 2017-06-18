65cacec1251a55d001c9f94336d5fa562dcc92fe
RaphaelReiter <rdso.reiter@gmail.com>
2017-06-18 14:45:22 +0200

    Firebase get complete

--- firstSocialMediaApp/FeedVC.swift
+++ firstSocialMediaApp/FeedVC.swift
@@ -15,15 +15,32 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
      
-        DataService.ds.REF_POSTS.observe(.value) { (snapshot) in
-            print(snapshot.value as Any)
-        }
+        DataService.ds.REF_POSTS.observeSingleEvent(of: .value, with: { (snapshot) in
+          
+            self.posts = []
+            
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+            
+        })
+        
         
     }
     
@@ -32,10 +49,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+       
+        return posts.count
     }
  
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("RAPHAEL.R: \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
